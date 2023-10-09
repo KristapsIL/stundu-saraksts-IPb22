a0396331f05b946d4436ec481ae1be1b5081d200
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-09 13:50:43 +0300

    pog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import Diena from './Diena';
 
 function App() {
-  const visasStundas = {
-    Pirmdiena:[
-      "Sports",
+  const visasStundas = [
+    {diena:"Pirmdiena",
+    stundas:[
+    "Sports",
     "Dabaszinības",
     "Vēsture",
     "Krievu valoda"
-    ],
-    Otrdiena:[
+    ]},
+    {diena:"Otrdiena",
+      stundas :[
       "Math",
       "Dabaszinības",
       "dator sistemas",
-    ]
-  }
+    ]},
+    {diena:"Tresdiena",
+      stundas :[
+      "Lat",
+      "Prog",
+      "Vest",
+      "Sport"
+    ]},
+    {diena:"Ceturtdiena",
+      stundas :[
+      "Math",
+      "Dabaszinības",
+      "aaaa",
+      "Sleepy"
+    ]},
+    {diena:"Piekdiena",
+      stundas :[
+      "Math",
+      "Dabaszinības",
+      "dator sistemas",
+    ]},
+  ]
+  const dienaJSX = visasStundas.map((diena, indekss)=>{
+    return <Diena diena={indekss} diena={diena.diena} stundas={diena.stundas}/>
+  })
+
+
   return( 
   <>
   <div>
-      <Diena diena="Pirmdiena" stundas={pirmdienasStundas}/>
-      <Diena diena="Otrdiena" stundas={otrdienasStundas}/>
+    {dienaJSX}
   </div>
   </>
   );
